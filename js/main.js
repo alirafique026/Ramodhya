cbdf75f57d94ec1b1b05c3abfcf869e0c09e4ece
ali <ali.rafique.026@gmail.com>
2021-09-30 12:26:12 +0530

    added slick slider

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$('.cent').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: false,
+    infinite: true,
+    arrows: false,
+});
+$('.property').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: true,
+    infinite: true,
+    arrows: true,
+})
+})
