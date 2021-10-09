b14e74a1afd8dd2de48595acb56e300a1b184104
ali <ali.rafique.026@gmail.com>
2021-10-09 13:15:43 +0530

    Vikrant sir changes

--- js/plugins/owl.js
+++ js/plugins/owl.js
@@ -3,7 +3,7 @@
     /* portfolio h1 */
         $("#portfolio-h1").owlCarousel({
             autoplay:true,
-            autoplayTimeout:1000,
+            autoplayTimeout:3000,
             loop:true,
             items : 1,
             dots:true,
